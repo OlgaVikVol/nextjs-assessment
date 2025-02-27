de7a5ec98e37a88acc30fa1d18abb88eda75e129
Olga Volobueva <olvik1605@gmail.com>
2025-02-26 22:15:42 -0500

    feat: add Sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import { SidebarProps } from './Sidebar.props';
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps) => {
-  return <div {...props}>Sidebar</div>;
+export const Sidebar = ({ className, ...props }: SidebarProps) => {
+  return (
+    <div className={cn(styles.sidebar, className)} {...props}>
+      Sidebar
+    </div>
+  );
 };
