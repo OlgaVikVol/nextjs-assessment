d6456f167615b5e7dde7e9e1ac79d5bdad44ddcb
Olga Volobueva <olvik1605@gmail.com>
2025-02-26 21:53:05 -0500

    feat: add Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,18 @@
 import { FooterProps } from './Footer.props';
+import cn from 'classnames';
+import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>© {format(new Date(), 'yyyy')} All rights reserved.</div>
+      <a href="#" target="_blank">
+        User agreement
+      </a>
+      <a href="#" target="_blank">
+        Privacy policy
+      </a>
+    </footer>
+  );
 };
