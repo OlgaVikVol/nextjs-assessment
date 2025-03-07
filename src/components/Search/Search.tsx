f25dc6f8cb0c071d6facf623789a8594b8d92253
Olga Volobueva <olvik1605@gmail.com>
2025-03-06 19:42:18 -0500

    feat: update Search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,23 +1,48 @@
-import { JSX, useState } from 'react';
+import { JSX, useState, KeyboardEvent } from 'react';
 import { SearchProps } from './Search.props';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import GlassIcon from './glass.svg';
 import cn from 'classnames';
 import styles from './Search.module.css';
+import { Input } from '../Input/Input';
+import { Button } from '../Button/Button';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
   const [search, setSearch] = useState('');
   const router = useRouter();
 
-  const goToSearch = () => {};
+  const goToSearch = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        q: search,
+      },
+    });
+  };
 
-  const handleKeyDown = () => {};
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      goToSearch();
+    }
+  };
 
   return (
-    <form
-      className={cn(className, styles.search)}
-      {...props}
-      role="search"
-    ></form>
+    <form className={cn(className, styles.search)} {...props} role="search">
+      <Input
+        className={styles.input}
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <Button
+        appearance="primary"
+        className={styles.button}
+        onClick={goToSearch}
+        aria-label="Search"
+      >
+        <GlassIcon />
+      </Button>
+    </form>
   );
 };
--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -3,13 +3,13 @@ import cn from 'classnames';
 import styles from './Sidebar.module.css';
 import { Menu } from '../Menu/Menu';
 import Logo from '../logo.svg';
-
+import { Search } from '@/components';
 
 export const Sidebar = ({ className, ...props }: SidebarProps) => {
   return (
     <div className={cn(styles.sidebar, className)} {...props}>
       <Logo className={styles.logo} height="50px" width="170px" />
-
+      <Search />
       <Menu />
     </div>
   );
